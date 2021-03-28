2b9bdcdbc966a9670fc73848a5b15153e3175a80
Arpad <solar@hu.inter.net>
2021-03-28 14:22:45 +0200

    user service getAll

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  /* table */
+  serverAddress: string = 'http://localhost:3000/users';
+
+              /* table              */
+  constructor(private http: HttpClient) { }
+
+  /* table */
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.serverAddress}`);
+  }
 }
